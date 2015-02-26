8a62f9befdaa617209f525ca0a993130062944d9
Kenneth Nicholson <kenneth@smashingboxes.com>
2015-02-26 11:33:38 -0500

    adds loading of events

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,22 @@
 var app = angular.module('app', []);
 
-app.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
+// app constant that can be used site wide
+app.constant('API_URL', 'http://fake-co-calendar.herokuapp.com/api/v1/events?callback=JSON_CALLBACK')
+
+app.controller('AppCtrl', ['$scope', '$http', 'API_URL', function($scope, $http, API_URL){
+
+  var events = {
+    load: function(){
+      // load events
+      $http.jsonp(API_URL).success(function (data){
+        var events = data.events;
+
+        $scope.events = events.list;
+
+      });      
+    }
+  }
+
+  events.load();
+
 }]);
