718fe4d4f0a21e8840e3ecfdfb6f240a3721caf4
Kenneth Nicholson <kenneth@smashingboxes.com>
2015-02-26 11:36:35 -0500

    adds current time

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,12 @@ var app = angular.module('app', []);
 // app constant that can be used site wide
 app.constant('API_URL', 'http://fake-co-calendar.herokuapp.com/api/v1/events?callback=JSON_CALLBACK')
 
-app.controller('AppCtrl', ['$scope', '$http', 'API_URL', function($scope, $http, API_URL){
+app.controller('AppCtrl', ['$scope', '$http', 'API_URL', '$interval', function($scope, $http, API_URL, $interval){
+
+  // get current time
+  var currentTime = function(){
+    $scope.currentTime = new Date();
+  }
 
   var events = {
     load: function(){
@@ -18,5 +23,6 @@ app.controller('AppCtrl', ['$scope', '$http', 'API_URL', function($scope, $http,
   }
 
   events.load();
+  $interval(currentTime, 1000);
 
 }]);
